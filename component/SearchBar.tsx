5b8f84180ab0fa275db95bf66c8de07221ae0bfe
Sungwook Kim <sungwook@c1r6s3.42seoul.kr>
2023-05-07 16:51:25 +0900

    [Feat] add dropdown

--- component/SearchBar.tsx
+++ component/SearchBar.tsx
@@ -1,35 +1,30 @@
 import { useEffect, useRef, useState } from 'react';
 import styles from '../styles/SearchBar.module.scss';
+import DropDown from './DropDown';
 
 export default function SearchBar() {
   const [intraId, setIntraId] = useState<string>('');
-  const [dropdown, setDropDown] = useState<boolean>(false);
+  const [onFocus, setOnFocus] = useState<boolean>(false);
   const searchBarRef = useRef<HTMLDivElement>(null);
 
   const goDetail = () => {
     if (intraId.length === 0) return;
-    setDropDown(false);
   };
 
   const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     const keyword = e.target.value;
     setIntraId(keyword);
-    if (keyword.length > 0) {
-      setDropDown(true);
-    } else {
-      setDropDown(false);
-    }
-  };
-
-  const handleClickOutside = (event: MouseEvent) => {
-    if (
-      searchBarRef.current &&
-      !searchBarRef.current.contains(event.target as Node)
-    )
-      setDropDown(false);
   };
 
   useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (
+        searchBarRef.current &&
+        !searchBarRef.current.contains(e.target as Node)
+      ) {
+        setOnFocus(false);
+      }
+    };
     document.addEventListener('mousedown', handleClickOutside);
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
@@ -42,14 +37,15 @@ export default function SearchBar() {
         type="text"
         value={intraId}
         placeholder="search by intra id"
+        onFocus={() => setOnFocus(true)}
         onChange={inputHandler}
-        onFocus={() => setDropDown(true)}
         onKeyDown={(e) => {
           if (e.key === 'Enter') {
             goDetail();
           }
         }}
       />
+      <DropDown keyword={intraId} focus={onFocus} />
     </div>
   );
 }
