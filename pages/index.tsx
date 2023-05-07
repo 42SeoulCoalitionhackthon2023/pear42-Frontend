ca9ebc98a88e19b8b151e5b116a6e49f0fef4061
Sungwook Kim <sungwook@c1r6s3.42seoul.kr>
2023-05-07 19:35:51 +0900

    [Feat] add profile type

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,46 @@
-import { Inter } from 'next/font/google';
+import { useEffect, useState } from 'react';
 import styles from '/styles/Home.module.css';
 
+type TProfile = {
+	intraImage: string;
+	intraId: string;
+	blackhole: Date;
+	campus: string;
+	cursus: string;
+	week: number;
+	total: number;
+	feedbackLevel: number;
+	outstanding: number;
+	passRate: number;
+	wordCount: number;
+	feedbackTime: number;
+};
+
 export default function Home() {
-  return <div className={styles.main}></div>;
+const [profileInfo, setProfileInfo] = useState<TProfile>({
+	intraImage: '',
+	intraId: '',
+	blackhole: new Date(),
+	campus: '',
+	cursus: '',
+	week: 0,
+	total: 0,
+	feedbackLevel: 0,
+	outstanding: 0,
+	passRate: 0,
+	wordCount: 0,
+	feedbackTime: 0,
+	});
+
+	useEffect(() => {
+		fetch('/api/profile') // props를 받아 intradId로 api 요청 필요
+			.then((res) => res.json())
+			.then((data) => {
+				setProfileInfo(data);
+			});
+	}, []);
+
+  return (<div className={styles.main}>
+	<Profile info={profileInfo}/>
+  </div>);
 }
