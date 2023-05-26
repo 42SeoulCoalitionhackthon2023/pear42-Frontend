6aedff61d1e02f719133b5b3de64e3b21806ce0c
KKWANH <kwanho0096@gmail.com>
2023-05-26 19:21:39 +0900

    [Styles] #6 className style to scss code

--- component/ThemeToggler.tsx
+++ component/ThemeToggler.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useTheme } from 'next-themes';
-// import { styles } from '/styles/ThemeToggler.module.scss';
+import { styles } from './ThemeToggler.module.scss';
 
 const ThemeToggler = () => {
 	const { systemTheme, theme, setTheme } = useTheme();
@@ -9,7 +9,7 @@ const ThemeToggler = () => {
 	return (
 		<button
 			onClick={() => theme == "dark" ? setTheme('light') : setTheme("dark")}
-			className='bg-gray-800 dark:bg-gray-50 hover:bg-gray-600 dark:hover:bg-gray-300 transition-all duration-100 text-white dark:text-gray-800 px-8 py-2 text-2xl md:text-4xl rounded-lg absolute bottom-32'
+			className='ThemeToggler'
 		>
 			Toggle Mode
 		</button>
